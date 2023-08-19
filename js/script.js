74ed8d670fd1cc4246d169b5c21135939370c2d5
Nishantphule <nishantphule12@gmail.com>
2023-08-19 20:13:37 +0530

    projects update

--- js/script.js
+++ js/script.js
@@ -2,36 +2,36 @@
 // skills section
 const skills = [
     {
-        skill:"Javascript",
-        img:"https://cdn-images-1.medium.com/max/2000/1*6ahbWjp_g9hqhaTDSJOL1Q.png"
+        skill: "Javascript",
+        img: "https://cdn-images-1.medium.com/max/2000/1*6ahbWjp_g9hqhaTDSJOL1Q.png"
     },
     {
-        skill:"HTML5",
-        img:"https://th.bing.com/th/id/OIP.bI8KDjd8-nDvzTX_Uok7FwHaHa?pid=ImgDet&rs=1"
+        skill: "HTML5",
+        img: "https://th.bing.com/th/id/OIP.bI8KDjd8-nDvzTX_Uok7FwHaHa?pid=ImgDet&rs=1"
     },
     {
-        skill:"CSS3",
-        img:"https://th.bing.com/th/id/OIP.xhnzhHcorvOTDzjLVg0LAwHaI4?pid=ImgDet&rs=1"
+        skill: "CSS3",
+        img: "https://th.bing.com/th/id/OIP.xhnzhHcorvOTDzjLVg0LAwHaI4?pid=ImgDet&rs=1"
     },
     {
-        skill:"Bootstrap",
-        img:"https://th.bing.com/th/id/OIP.KJPH_yiM2ubGn9DLOmjCtAHaFj?pid=ImgDet&rs=1"
+        skill: "Bootstrap",
+        img: "https://th.bing.com/th/id/OIP.KJPH_yiM2ubGn9DLOmjCtAHaFj?pid=ImgDet&rs=1"
     },
     {
-        skill:"React.JS",
-        img:"https://nickroberts.ninja/wp-content/uploads/2017/07/react.jpg"
+        skill: "React.JS",
+        img: "https://nickroberts.ninja/wp-content/uploads/2017/07/react.jpg"
     },
     {
-        skill:"Node.JS",
-        img:"https://1.bp.blogspot.com/-mzw13XQJPYM/XgzNHXSUdXI/AAAAAAAAAYY/xeIhLBEpTQUn8huUCnWXdUX6vIR_T4UCQCPcBGAYYCw/s1600/http___pluspng.com_img-png_nodejs-png-nodejs-icon-png-50-px-1600.png"
+        skill: "Node.JS",
+        img: "https://1.bp.blogspot.com/-mzw13XQJPYM/XgzNHXSUdXI/AAAAAAAAAYY/xeIhLBEpTQUn8huUCnWXdUX6vIR_T4UCQCPcBGAYYCw/s1600/http___pluspng.com_img-png_nodejs-png-nodejs-icon-png-50-px-1600.png"
     },
     {
-        skill:"MongoDB",
-        img:"https://th.bing.com/th/id/OIP.Ie_MJg_cy7B7gf4Uo7qP6wHaD4?w=290&h=180&c=7&r=0&o=5&pid=1.7"
+        skill: "MongoDB",
+        img: "https://th.bing.com/th/id/OIP.Ie_MJg_cy7B7gf4Uo7qP6wHaD4?w=290&h=180&c=7&r=0&o=5&pid=1.7"
     },
     {
-        skill:"Python",
-        img:"https://th.bing.com/th/id/OIP.EDJ9xoErBbZqK2tExVoJfAHaHY?w=234&h=220&c=7&r=0&o=5&pid=1.7"
+        skill: "Python",
+        img: "https://th.bing.com/th/id/OIP.EDJ9xoErBbZqK2tExVoJfAHaHY?w=234&h=220&c=7&r=0&o=5&pid=1.7"
     },
 ]
 
@@ -40,17 +40,17 @@ const skillSection = document.querySelector(".skills-section")
 
 skills.forEach((skill) => {
     const skillCard = document.createElement("div")
-    skillCard.className += "skill-card" 
-    
+    skillCard.className += "skill-card"
+
     const skillName = document.createElement("h4")
     skillName.className += "skill-title"
     skillName.innerHTML = skill.skill
-    
+
     const skillImg = document.createElement("img")
     skillImg.className += "skill-img"
     skillImg.src = skill.img
-    
-    skillCard.append(skillImg,skillName)
+
+    skillCard.append(skillImg, skillName)
     skillSection.appendChild(skillCard)
 })
 
@@ -59,26 +59,35 @@ skills.forEach((skill) => {
 
 const projects = [
     {
-        name:"Youtube-Clone",
-        lang:"Javascript,HTML,CSS & bootstrap",
-        webLink:"https://youtube-clone-bynishant.netlify.app/"
+        name: "Youtube-Clone",
+        lang: "Javascript,HTML,CSS & bootstrap",
+        webLink: "https://youtube-clone-bynishant.netlify.app/"
     },
     {
-        name:"Weather Forecast App",
-        lang:"Javascript,HTML,CSS & bootstrap",
-        webLink:"https://weather-app-api-openweather.netlify.app/"
+        name: "Weather Forecast App",
+        lang: "Javascript,HTML,CSS & bootstrap",
+        webLink: "https://weather-app-api-openweather.netlify.app/"
     },
     {
-        name:"React-crud-user-App",
-        lang:"React,Javascript,CSS,mockapi & Material UI",
-        webLink:"https://react-app-user-crud.netlify.app/users"
+        name: "React-crud-user-App",
+        lang: "React,CSS,mockapi & Material UI",
+        webLink: "https://react-app-user-crud.netlify.app/users"
     },
     {
-        name:"ToDo-App",
-        lang:"Javascript,HTML,CSS & bootstrap",
-        webLink:"https://todo-list-myapp.netlify.app/"
+        name: "React-crud-management-App",
+        lang: "React,CSS & Material UI",
+        webLink: "https://crud-management-react-app.netlify.app/"
+    },
+    {
+        name: "Hotel Menu Site for QR Code",
+        lang: "React,Node & Express deployed on Lambda AWS & Material UI",
+        webLink: "https://greeninn.netlify.app/"
+    },
+    {
+        name: "Password Reset in MERN stack with nodemailer Package",
+        lang: "React,Node & Express & Material UI",
+        webLink: "https://password-reset-react-app.netlify.app/"
     }
-    
 ]
 
 const projectsArea = document.getElementById("projects")
